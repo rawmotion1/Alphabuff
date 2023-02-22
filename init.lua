4a63286b46f22626f766a863ab1cbe592d4c59ac
rawmotion1 <me.raw.github@spfict.com>
2023-02-22 11:04:23 +0100

    feat: Completely redesigned and refactored - More compact and easier to read - Buff countdown timers - Buff color coding - Enhanced performance - Fixed sorting glitches

--- init.lua
+++ init.lua
@@ -1,179 +1,185 @@
 --Alphabuff.lua
 --by Rawmotion
---v 1.0.0
+local version = '1.5.0'
 ---@type Mq
 local mq = require('mq')
 ---@type ImGui
 require('ImGui')
 
-local current_sort_specs = nil
-local function CompareWithSortSpecs(a, b)
-    for n = 1, current_sort_specs.SpecsCount, 1 do
-        local sort_spec = current_sort_specs:Specs(n)
-        local delta = 0
-        if sort_spec.ColumnIndex == 0 then
-            delta = a.slot - b.slot
-        elseif sort_spec.ColumnIndex == 1 then
-            if a.name < b.name then
-                delta = -1
-            elseif b.name < a.name then
-                delta = 1
-            else
-                delta = 0
-            end
-        end
-        if delta ~= 0 then
-            if sort_spec.SortDirection == ImGuiSortDirection.Ascending then
-                return delta < 0
-            end
-            return delta > 0
-        end
+local sortedBy
+local function sortName(a, b)
+    sortedBy = 'name'
+    local delta = 0
+    if a.name < b.name then
+        delta = -1
+    elseif b.name < a.name then
+        delta = 1
+    else
+        delta = 0
+    end
+    if delta ~= 0 then
+        return delta < 0
     end
     return a.slot - b.slot < 0
+end
+
+local function sortSlot(a, b)
+    sortedBy = 'slot'
+    local delta = 0
+    delta = a.slot - b.slot
+    if delta ~= 0 then
+        return delta < 0
+    end
+    return a.slot - b.slot < 0
+end
+
+local function calcDuration(b)
+    local winner
+    local current = mq.TLO.Me.Buff(b).Duration() or 0
+    local scurrent = mq.TLO.Me.Buff(b).MyDuration() or 0
+    current = current / 1000 --to seconds
+    scurrent = scurrent * 6 --to seconds
+    winner = math.max(current, scurrent)
+    return winner
+end
+
+local color
+local function calcRatio(s,d)
+    local ratio
+    local current = mq.TLO.Me.Buff(s).Duration() or 0
+    local scurrent = mq.TLO.Me.Buff(s).MyDuration() or 0
+    current = current / 1000 --to seconds
+    scurrent = scurrent * 6 --to seconds
+    if scurrent == -6 or scurrent > 36000 then --Permanent, or close enough
+        ratio = 1
+        color = 'gray'
+    elseif scurrent <= 1200 then --a short buff that lasts less than 20 min
+        ratio = current / d
+        color = 'green'
+    elseif current / 60 > 20 then --a buff that currently has more than 20 minutes shows 100%
+        ratio = 1
+        color = 'blue'
+    else --a buff that lasts more than 20 minutes but has less that 20min remaining
+        ratio = (current / 60) / 20
+        color = 'blue'
+    end
+    return ratio
 end
 
 local buffs = {}
 local function loadBuffs()
     for i = 1,42 do
-        local setTime
-        local hours = mq.TLO.Me.Buff(i).Duration.Hours()
-        local minutes = mq.TLO.Me.Buff(i).Duration.Minutes()
-        local seconds = mq.TLO.Me.Buff(i).Duration.Seconds()
-        local totalTime = mq.TLO.Me.Buff(i).Duration.TimeHMS()
-        if hours == nil or minutes == nil or seconds == nil then setTime = 'x'
-        elseif hours < 1 and minutes < 1 then setTime = seconds..'s'
-        elseif hours < 1 then setTime = minutes..'m'
-        elseif hours > 24 then setTime = '\xee\xac\xbd'
-        else setTime = hours..'h'
+        local name
+        local duration --seconds
+        if mq.TLO.Me.Buff(i)() then
+            name = mq.TLO.Me.Buff(i)()
+            if mq.TLO.Me.Buff(i).Duration() < 300 then
+                duration = 0
+            else
+                duration = calcDuration(i)
+            end
+        else
+            name = 'zz'
+            duration = 0
         end
         local buff = {
             slot = i,
-            name = mq.TLO.Me.Buff(i)(),
-            icon = mq.TLO.Me.Buff(i).SpellIcon(),
-            time = setTime,
-            timeHMS = totalTime
-        }
+            name = name,
+            duration = duration }
         table.insert(buffs, buff)
-        if buff.name == nil then buff.name = 'zz' end
     end
 end
 loadBuffs()
 
-local makeDirty
 local function updateBuffs()
     for k,v in pairs(buffs) do
-        local setTime
-        local hours = mq.TLO.Me.Buff(v.slot).Duration.Hours()
-        local minutes = mq.TLO.Me.Buff(v.slot).Duration.Minutes()
-        local seconds = mq.TLO.Me.Buff(v.slot).Duration.Seconds()
-        local totalTime = mq.TLO.Me.Buff(v.slot).Duration.TimeHMS()
-        if hours == nil or minutes == nil or seconds == nil then setTime = 'x'
-        elseif hours < 1 and minutes < 1 then setTime = seconds..'s'
-        elseif hours < 1 then setTime = minutes..'m'
-        elseif hours > 24 then setTime = '\xee\xac\xbd'
-        else setTime = hours..'h'
+        if mq.TLO.Me.Buff(v.slot)() then
+            if v.name ~= mq.TLO.Me.Buff(k)() then
+                v.name = mq.TLO.Me.Buff(v.slot)()
+                if mq.TLO.Me.Buff(v.slot).Duration() < 300 then
+                    v.duration = 0
+                else
+                    v.duration = calcDuration(v.slot)
+                end
+                if sortedBy == 'name' then table.sort(buffs, sortName) else table.sort(buffs, sortSlot) end
+            end
+        else
+            if v.name ~= 'zz' then
+                v.name = 'zz'
+                v.duration = 0
+                if sortedBy == 'name' then table.sort(buffs, sortName) else table.sort(buffs, sortSlot) end
+            end
         end
-        if mq.TLO.Me.Buff(v.slot)() ~= nil then buffs[k].name = mq.TLO.Me.Buff(v.slot)()
-        else buffs[k].name = 'zz'
-        end
-        buffs[k].icon = mq.TLO.Me.Buff(v.slot).SpellIcon()
-        buffs[k].time = setTime
-        buffs[k].timeHMS = totalTime
-        makeDirty = true
     end
 end
 
 local Open, ShowUI = true, true
 local anim = mq.FindTextureAnimation('A_SpellIcons')
-local tableSorting_flags = bit32.bor(ImGuiTableFlags.Sortable, ImGuiTableFlags.RowBg, ImGuiTableFlags.NoPadOuterX, ImGuiTableFlags.NoBordersInBody)
-
 local function buildWindow()
-    ImGui.SetWindowSize(220, 970, ImGuiCond.Once)
-    
-    ImGui.BeginTable("table_sorting", 3, tableSorting_flags)
-    ImGui.PushStyleVar(ImGuiStyleVar.FramePadding, 0,2)
-    ImGui.TableSetupColumn("#       ", bit32.bor(ImGuiTableColumnFlags.DefaultSort, ImGuiTableColumnFlags.WidthFixed),8)
-    ImGui.TableSetupColumn("Name", bit32.bor(ImGuiTableColumnFlags.PreferSortAscending, ImGuiTableColumnFlags.WidthStretch),1)
-    ImGui.TableSetupColumn("\xee\xa2\xb5", bit32.bor(ImGuiTableColumnFlags.NoSort, ImGuiTableColumnFlags.WidthFixed),24)
-    ImGui.TableSetupScrollFreeze(0, 1)
-    local sort_specs = ImGui.TableGetSortSpecs()
-    if sort_specs.SpecsDirty or makeDirty == true then
-        for n = 1, sort_specs.SpecsCount, 1 do local sort_spec = sort_specs:Specs(n) end
-        current_sort_specs = sort_specs
-        table.sort(buffs, CompareWithSortSpecs)
-        current_sort_specs = nil
-        sort_specs.SpecsDirty = false
-        makeDirty = false
-    end
-    
-    ImGui.TableHeadersRow()
-    
+    ImGui.SetWindowSize(200, 925, ImGuiCond.Once)
+    local x,y = ImGui.GetContentRegionAvail()
+    local halfWidth = x/2
+    ImGui.SetWindowFontScale(1)
+    ImGui.PushStyleVar(ImGuiStyleVar.FramePadding, 0,0)
+    ImGui.PushStyleVar(ImGuiStyleVar.ItemSpacing, 1, 1)
+    if ImGui.Button('By slot', halfWidth, 20) then table.sort(buffs, sortSlot) end
+    ImGui.SameLine()
+    if ImGui.Button('By name', halfWidth, 20) then table.sort(buffs, sortName) end
+    ImGui.PopStyleVar(2)
     for k,v in pairs(buffs) do
         local buff = buffs[k]
-        ImGui.PushID(buff)
-        
-        ImGui.TableNextRow()
-        ImGui.TableNextColumn()    
-     
-        ImGui.AlignTextToFramePadding() ImGui.SetWindowFontScale(.8)
-        ImGui.TextColored(1,1,1,.5,string.format("%02d", buff.slot))
-        if (ImGui.IsItemHovered()) and buff.name ~= 'zz' then
-            ImGui.SetTooltip(buff.name..' ('..buff.timeHMS..')')
+        ImGui.PushID(buff)      
+        local icon
+        local function getIcon()
+            if buff.name ~= 'zz' then
+                anim:SetTextureCell(mq.TLO.Me.Buff(buff.slot).SpellIcon())
+                icon = ImGui.DrawTextureAnimation(anim, 17, 17)
+            else
+                icon = ImGui.TextColored(1,1,1,.5,string.format("%02d", buff.slot))
+            end
+            return icon
         end
-
-        ImGui.TableNextColumn()
-
+        local ratio
+        if buff.name ~= nil and buff.duration ~= nil and buff.duration ~= 0 then
+        ratio = calcRatio(buff.slot, buff.duration)
+        else 
+            ratio = .05
+        end 
+        ImGui.BeginGroup()
         ImGui.PushStyleVar(ImGuiStyleVar.ItemSpacing, 1, 4)
-        anim:SetTextureCell(buff.icon)
-        if buff.icon ~= nil then ImGui.DrawTextureAnimation(anim, 17, 17) end
-        if (ImGui.IsItemHovered()) and buff.name ~= 'zz' then
-            ImGui.SetTooltip(buff.name..' ('..buff.timeHMS..')')
-        end
+        getIcon()
         ImGui.SameLine()
         if buff.name ~= 'zz' then
             if mq.TLO.Me.Buff(buff.slot).SpellType() == 'Detrimental' then
-                ImGui.AlignTextToFramePadding() ImGui.SetWindowFontScale(.9)
-                ImGui.TextColored(1, 0, 0, 1, buff.name)
+                ImGui.PushStyleColor(ImGuiCol.PlotHistogram, .7, 0, 0, .7)
+            elseif color == 'green' then
+                ImGui.PushStyleColor(ImGuiCol.PlotHistogram, .2, 1, 6, .4)
+            elseif color == 'gray' then
+                ImGui.PushStyleColor(ImGuiCol.PlotHistogram, 1, 1, 1, .2)
             else
-                ImGui.AlignTextToFramePadding() ImGui.SetWindowFontScale(.9)
-                ImGui.Text(buff.name)
+                ImGui.PushStyleColor(ImGuiCol.PlotHistogram, .2, .6, 1, .4)
             end
-            if (ImGui.IsItemHovered()) then
-                if buff.name ~= nil and buff.timeHMS ~= nil then
-                    ImGui.SetTooltip(buff.name..' ('..buff.timeHMS..')')
-                end
-            end
+            ImGui.ProgressBar(ratio, ImGui.GetContentRegionAvail(), 16, '##'..buff.name) 
+            ImGui.SetCursorPosY(ImGui.GetCursorPosY() - 21)
+            ImGui.SetCursorPosX(ImGui.GetCursorPosX() + 20)
+            ImGui.Text(buff.name)
+            ImGui.PopStyleColor()
         end
+        ImGui.PopStyleVar()
+        ImGui.EndGroup()
         if ImGui.IsItemClicked(ImGuiMouseButton.Left) then mq.cmdf('/removebuff %s', buff.name) end
         if ImGui.IsItemClicked(ImGuiMouseButton.Right) then mq.TLO.Me.Buff(buff.slot).Inspect() end
-        ImGui.PopStyleVar()
-        
-        ImGui.TableNextColumn()
-        
-        if buff.time ~= 'x' then
-            if mq.TLO.Me.Buff(buff.slot).SpellType() == 'Detrimental' then
-                ImGui.AlignTextToFramePadding() ImGui.SetWindowFontScale(.8)
-                ImGui.TextColored(1,0,0,1,buff.time)
-            else
-                ImGui.AlignTextToFramePadding() ImGui.SetWindowFontScale(.8)
-                ImGui.TextColored(.8,0.5,.0,1,buff.time)
-            end
-        end
-        
-        ImGui.SetWindowFontScale(.9)
+        local hms = mq.TLO.Me.Buff(buff.slot).Duration.TimeHMS() or 0
+        if (ImGui.IsItemHovered()) and buff.name ~= 'zz' then ImGui.SetTooltip(string.format("%02d", buff.slot)..' '..buff.name..' ('..hms..')') end
         ImGui.PopID()
-        
     end
-    ImGui.PopStyleVar()
-    ImGui.EndTable()
-    
 end
 
-local windowFlags = bit32.bor(ImGuiWindowFlags.NoTitleBar)
 local function ab()
-    ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, 0, 10)
-    Open, ShowUI = ImGui.Begin('Alphabuff', Open, windowFlags)
-    ImGui.PopStyleVar()
+    ImGui.PushStyleVar(ImGuiStyleVar.WindowPadding, 0, 1)
+    ImGui.PushStyleVar(ImGuiStyleVar.WindowRounding, 12)
+    Open, ShowUI = ImGui.Begin('Alphabuff '..version, Open)
+    ImGui.PopStyleVar(2)
     if ShowUI then buildWindow() end
     ImGui.End()
 end
@@ -183,6 +189,6 @@ mq.imgui.init('Alphabuff', ab)
 local terminate = false
 while not terminate do
     updateBuffs()
-    mq.delay(500)
+    mq.delay(250)
 	if not Open then return end
 end
